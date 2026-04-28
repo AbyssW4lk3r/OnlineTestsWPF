ddf2c0da927aa67092449cb0a31e3614ffaa3868
AbyssW4lk3r <pn41967@gmail.com>
2026-04-28 07:42:57 +0500

    Add files via upload

---
+++ OnlineTests/Models/Models.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineTests.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Login { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class TestView
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string MaxScore { get; set; }
+        public int QuestionCount { get; set; }
+    }
+
+    public class Question
+    {
+        public int Id { get; set; }
+        public int TestId { get; set; }
+        public string QuestionText { get; set; }
+        public string QuestionType { get; set; }
+    }
+
+    public class Answer
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+
+    public class StudentView
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Login { get; set; }
+        public string Phone { get; set; }
+        public string BirthDate { get; set; }
+    }
+
+    public class ResultView
+    {
+        public string TestTitle { get; set; }
+        public string AttemptDate { get; set; }
+        public string Score { get; set; }
+        public string PercentCorrect { get; set; }
+    }
+
+    public class ResultAdminView
+    {
+        public string StudentName { get; set; }
+        public string TestTitle { get; set; }
+        public string AttemptDate { get; set; }
+        public string Score { get; set; }
+        public string PercentCorrect { get; set; }
+    }
+}
---
+++ OnlineTests/Windows/StudentWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using OnlineTests.Services;
+
+namespace OnlineTests.Windows
+{
+    public partial class StudentWindow : Window
+    {
+        private int userId;
+        private DatabaseService db = new DatabaseService();
+
+        public StudentWindow(int userId, string fullName)
+        {
+            InitializeComponent();
+            this.userId = userId;
+            WelcomeText.Text = $"Привет, {fullName}!";
+            LoadTests();
+            LoadResults();
+        }
+
+        private void LoadTests()
+        {
+            TestsListView.ItemsSource = db.GetAvailableTests();
+        }
+
+        private void LoadResults()
+        {
+            ResultsListView.ItemsSource = db.GetStudentResults(userId);
+        }
+
+        private void TestsListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selected = TestsListView.SelectedItem as Models.Test;
+            if (selected != null)
+            {
+                new TestPassingWindow(userId, selected.Id, selected.Title).ShowDialog();
+                LoadResults();
+            }
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            new LoginWindow().Show();
+            this.Close();
+        }
+    }
+}
